81c859d694e19c18e45f2f21ae6c25fee5aca216
root <root@localhost.localdomain>
2021-03-23 23:28:19 +0530

    tracking chmod as well and registering path in watcher

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ copy_files:
 	cp -r ./src Docker/.
 
 build:  copy_files
-	hack/build-component-image.sh Docker openshift/go-log-file-watcher-driver-v0
+	#hack/build-component-image.sh Docker openshift/go-log-file-watcher-driver-v0
+	hack/build-component-image.sh Docker  docker.io/cognetive/go-log-file-watcher-with-symlink-support-v1
--- src/log-file-watcher.go
+++ src/log-file-watcher.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
+	"math"
+	"time"
 	"net/http"
 	"os"
+//	"os/exec"
 	"path/filepath"
         "strings"
 	"github.com/fsnotify/fsnotify"
@@ -22,10 +26,112 @@ func debug(f string, x ...interface{}) {
 	}
 }
 
+
+
+//////////////////////
+type Event = fsnotify.Event
+type Op = fsnotify.Op
+
+const (
+	Create Op = fsnotify.Create
+	Write     = fsnotify.Write
+	Remove    = fsnotify.Remove
+	Rename    = fsnotify.Rename
+	Chmod     = fsnotify.Chmod
+)
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Watcher is like fsnotify.Watcher but also notifies on changes to symlink targets
+
+
+// Event returns the next event.
+func (w *FileWatcher) Event() (e Event, err error) {
+	return w.EventTimeout(time.Duration(math.MaxInt64))
+}
+
+// EventTimeout returns the next event or os.ErrDeadlineExceeded if timeout is exceeded.
+func (w *FileWatcher) EventTimeout(timeout time.Duration) (e Event, err error) {
+	var ok bool
+	select {
+	case e, ok = <-w.watcher.Events:
+	case err, ok = <-w.watcher.Errors:
+	case <-time.After(timeout):
+		return Event{}, os.ErrDeadlineExceeded
+	}
+	switch {
+	case !ok:
+		return Event{}, io.EOF
+	case e.Op == Create:
+		debug("e.Op is Create for e.Name %v", e.Name)
+		if info, err := os.Lstat(e.Name); err == nil {
+			if isSymlink(info) {
+				debug("watcher.Add %v",e.Name)
+				_ = w.watcher.Add(e.Name)
+			}
+		}
+	case e.Op == Remove:
+		debug("e.Op is Remove for e.Name %v", e.Name)
+		w.watcher.Remove(e.Name)
+	case e.Op == Chmod || e.Op == Rename:
+		debug("e.Op - Rename or Chmod for %v %v",e.Op, e.Name)
+		if info, err := os.Lstat(e.Name); err == nil {
+			if isSymlink(info) {
+				// Symlink target may have changed.
+			//	rawfilename := os.Readlink(e.Name)
+				_ = w.watcher.Remove(e.Name)
+				//os.Create(rawfilename)
+				_ = w.watcher.Add(e.Name)
+			}
+		}
+	}
+	return e, err
+}
+
+// Add a file to the watcher
+func (w *FileWatcher) Add(name string) error {
+	if err := w.watcher.Add(name); err != nil {
+		return err
+	}
+	w.added[name] = true // Explicitly added, don't auto-Remove
+
+	// Scan directories for existing symlinks, we wont' get a Create for those.
+	if infos, err := ioutil.ReadDir(name); err == nil {
+		for _, info := range infos {
+			if isSymlink(info) {
+				debug("Is a symlink")
+				_ = w.watcher.Add(filepath.Join(name, info.Name()))
+			}
+		}
+	}
+	return nil
+}
+
+// Remove name from watcher
+func (w *FileWatcher) Remove(name string) error {
+	delete(w.added, name)
+	return w.watcher.Remove(name)
+}
+
+// Close watcher
+func (w *FileWatcher) Close() error { return w.watcher.Close() }
+
+func isSymlink(info os.FileInfo) bool {
+	return (info.Mode() & os.ModeSymlink) == os.ModeSymlink
+}
+//////////////////////
+
+
+
 type FileWatcher struct {
 	watcher *fsnotify.Watcher
 	metrics *prometheus.CounterVec
 	sizes   map[string]float64
+	added map[string]bool
 }
 
 
@@ -33,6 +139,7 @@ func NewFileWatcher(dir string) (*FileWatcher, error) {
 	var namespace string
 	var podname string
 	var containerid string
+	var completepathoffile string
 
 	debug("watching %v", dir)
 	w := &FileWatcher{
@@ -41,12 +148,16 @@ func NewFileWatcher(dir string) (*FileWatcher, error) {
 			Help: "total bytes logged to disk (log file) ",
 		}, []string{"path"}),
 		sizes: make(map[string]float64),
+		added: make(map[string]bool),
 	}
 	defer prometheus.Register(w.metrics)
 	var err error
 	if w.watcher, err = fsnotify.NewWatcher(); err == nil {
-		err = w.watcher.Add(dir)
+		//defer w.watcher.Close()
+		err = w.Add(dir)
 	}
+	w.added[dir] = true
+
 	if err != nil {
 		return nil, err
 	}
@@ -59,6 +170,7 @@ func NewFileWatcher(dir string) (*FileWatcher, error) {
 	}
 	for _, file := range files {
 		filename := file.Name()
+                if (strings.Contains(filename,".log")) {
 		filenameslice := strings.Split(filename,"_")
 		if (len(filenameslice) == 3) {
 		namespace = filenameslice[0]
@@ -72,11 +184,18 @@ func NewFileWatcher(dir string) (*FileWatcher, error) {
 
 
 		debug("namespace = %v podname = %v containerid = %v",namespace, podname, containerid)
-		err := w.Update(filepath.Join(dir, file.Name()))
+		
+		completepathoffile=filepath.Join(dir, file.Name())
+		rawPath, err := filepath.EvalSymlinks(completepathoffile)
+		if (err == nil) {
+		err := w.Update(completepathoffile)
+		debug("simpathname = %v rawpathname = %v",filename, rawPath)
 		if err != nil {
 			debug("error in update: %v", err)
 		}
 	}
+	}
+     }
 	return w, nil
 }
 
@@ -108,23 +227,12 @@ func (w *FileWatcher) Update(path string) error {
 }
 
 func (w FileWatcher) Watch() {
+	
 	for {
-		var err error
-		select {
-		case e := <-w.watcher.Events:
-			debug("event %v", e)
-			if e.Op == fsnotify.Write {
-				// Write (which includes truncate) is the only operation that can change file size.
-				w.Update(e.Name)
-			}
-			if err != nil && !os.IsNotExist(err) {
-				debug("watch error: %v", err)
-			}
-		case err = <-w.watcher.Errors:
-			if err != nil {
-				debug("watch error: %v", err)
-			}
-		}
+		e, err := w.Event()
+		fatal(err)
+		log.Print(e)
+		w.Update(e.Name)
 	}
 }
 
@@ -144,6 +252,7 @@ func main() {
 
 	w, err := NewFileWatcher(dir)
 	if err != nil {
+		debug("NewFileWatcher error")
 		log.Fatal(err)
 	}
 	go w.Watch()
